c66cdfe4d353cbb7eb71eaf2c38acca1e326a6d5
lay <1581133593@qq.com>
2021-08-17 20:58:23 +0800

    fix(components): fix bug

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {},
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style scoped lang="less"></style>
