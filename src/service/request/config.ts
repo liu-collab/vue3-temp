9bbb80692ea1f9bc1c201974bc09636447bb6233
lay <1581133593@qq.com>
2021-09-06 15:25:08 +0800

    docs(service): init

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -2,11 +2,11 @@ let BASE_URL = ''
 const TIME_OUT = 5000
 
 if (process.env.NODE_ENV === "production") {
-  BASE_URL = "http://www.baidu.com/"
+  BASE_URL = "http://www.xxx.com/"
 } else if (process.env.NODE_ENV === "development") {
-  BASE_URL = "http://www.baidu.com/"
+  BASE_URL = "http://www.xxx.com/"
 } else {
-  BASE_URL = "http://www.baidu.com/"
+  BASE_URL = "http://www.xxx.com/"
 
 }
 export { BASE_URL, TIME_OUT }
